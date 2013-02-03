e8dd6a4ed2d466e3aa2f53645249a71af584ed23
Andreas Stührk <andy-python@hammerhartes.de>
2013-02-03 21:35:08 +0100

    Added the possibility to execute a query using "F5" or "C-c C-c".
    Also added a hint that "C-x C-c" exits (appears after one pressed "C-c")

--- eekhoorn/reader.py
+++ eekhoorn/reader.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from colors import red
+from colors import green, red
 from pyrepl import commands, completing_reader
 from pyrepl.historical_reader import HistoricalReader
 from pyrepl.reader import SYNTAX_WORD
@@ -26,6 +26,26 @@ class complete(completing_reader.complete):
         else:
             return super(complete, self).do()
 
+class maybe_forced_accept(commands.Command):
+    """Prints a hint that you can exit using "C-x C-c" or accepts the
+    query if executed twice.
+    """
+
+    def do(self):
+        reader = self.reader
+        if reader.last_command_is(reader.commands["maybe-forced-accept"]):
+            self.finish = True
+        else:
+            reader.msg = green('Press C-x C-c to exit, C-c to execute query')
+            reader.dirty = True
+
+class quit(commands.Command):
+    def do(self):
+        self.reader.update_screen()
+        self.reader.console.finish()
+        raise EOFError
+
+
 class Reader(
         CompletingReader,
         HistoricalReader,
@@ -37,10 +57,16 @@ class Reader(
         self.ps2 = "sql> "
         self.ps3 = "  -> "
         self.ps4 = "  -> "
-        self.commands["complete"] = complete
-        self.commands["maybe-accept"] = maybe_accept
+        self._add_commands()
         self._add_keybindings()
         self._init_syntax_table()
+
+    def _add_commands(self):
+        self.commands["complete"] = complete
+        self.commands["maybe-accept"] = maybe_accept
+        self.commands["maybe-forced-accept"] = maybe_forced_accept
+        self.commands["finish"] = commands.FinishCommand
+        self.commands["quit"] = quit
 
     def _add_keybindings(self):
         """This is a hack, but pyrepl's keymap stuff seems to be
@@ -60,13 +86,13 @@ class Reader(
         self.syntax_table[u("_")] = SYNTAX_WORD
 
     def collect_keymap(self):
-        keymap = super(Reader, self).collect_keymap()
-        return keymap + (
-            (r'\n', 'maybe-accept'),
-            # XXX not working yet
-            (r'\M-b', 'backward-word'),
-            (r"\M-f", "forward-word"),
-        )
+        for (key, command) in super(Reader, self).collect_keymap():
+            if key != r'\C-c':
+                yield (key, command)
+        yield (r'\n', 'maybe-accept')
+        yield (r'\C-c', 'maybe-forced-accept')
+        yield (r'\<f5>', 'finish')
+        yield (r'\C-x\C-c', 'quit')
 
     def error(self, msg):
         "More pretty error messages."
