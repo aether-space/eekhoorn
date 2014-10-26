67362e81061dec9079d7ec5865cda0b6578b15ac
Andreas Stührk <andy@hammerhartes.de>
2014-10-26 19:48:20 +0100

    Always require setuptools.

--- setup.py
+++ setup.py
@@ -1,32 +1,8 @@
 # encoding: utf-8
 
-try:
-    from setuptools import setup
-    has_setuptools = True
-except ImportError:
-    from distutils.core import setup
-    has_setuptools = False
+from setuptools import setup
 
 from eekhoorn import __version__
-
-
-if has_setuptools:
-    extra_kwargs = {
-        "entry_points": {
-            "console_scripts": [
-                "eekhoorn = eekhoorn.__main__:main"
-            ]
-        },
-        "install_requires": [
-            "ansicolors",
-            "pyrepl",
-            "six",
-            "sqlalchemy >= 0.7",
-            "sqlparse"
-        ]
-    }
-else:
-    extra_kwargs = {}
 
 
 setup(
@@ -34,4 +10,15 @@ setup(
     version=__version__,
     description="A fancy SQL console",
     packages=["eekhoorn", "eekhoorn.tests"],
-    **extra_kwargs)
+    entry_points={
+        "console_scripts": [
+            "eekhoorn = eekhoorn.__main__:main"
+        ]
+    },
+    install_requires=[
+        "ansicolors",
+        "pyrepl",
+        "six",
+        "sqlalchemy >= 0.7",
+        "sqlparse"
+    ])
